9c3fdf8b79d0b2179730e4241669a68457a75e0c
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-10-19 17:07:06 -0300

    Criação da migration de cars

---
+++ src/migrations/1666209695110-CreateCars.ts
@@ -0,0 +1,74 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCars1666209695110 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'cars',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'daily_rate',
+            type: 'numeric',
+          },
+          {
+            name: 'available',
+            type: 'boolean',
+          },
+          {
+            name: 'license_plate',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'fine_amount',
+            type: 'numeric',
+          },
+          {
+            name: 'brand',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'category_id',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ['category_id'],
+            referencedTableName: 'categories',
+            referencedColumnNames: ['id'],
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('cars');
+  }
+}
