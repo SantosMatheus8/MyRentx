fc2674b5b047d0183887c31efea6777aa4c49476
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-10-19 16:06:28 -0300

    Criação da migration de user

---
+++ src/migrations/1666206104015-CreateUsers.ts
@@ -0,0 +1,61 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1666206104015 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+          },
+          {
+            name: 'nome',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            length: '18',
+          },
+          {
+            name: 'avatar',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'driver_license',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'is_admin',
+            type: 'boolean',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
