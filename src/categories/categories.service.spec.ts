b7338493c16a5969631e2eac87a3e55bd857f9ad
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-11-08 22:23:04 -0300

    Criando os casos de teste de categories

--- src/categories/categories.service.spec.ts
+++ src/categories/categories.service.spec.ts
@@ -3,6 +3,7 @@ import { Repository } from 'typeorm';
 import { CategoriesService } from './categories.service';
 import { Category } from './entities/category.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { NotFoundException } from '@nestjs/common';
 
 const category1 = new Category();
 category1.id = '1ca415c6-32be-488c-b7bf-12b8649c99bd';
@@ -48,5 +49,91 @@ describe('CategoriesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
     expect(repository).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('A category must be created', async () => {
+      const category = await service.create({
+        name: '4x4',
+        description: 'Category Description',
+      });
+
+      expect(category).toEqual(categoryList[0]);
+      expect(repository.create).toHaveBeenCalledTimes(1);
+      expect(repository.save).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findAll', () => {
+    it('Must be list all categories', async () => {
+      const categories = await service.findAll();
+
+      expect(categories).toEqual(categoryList);
+      expect(repository.find).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findOne', () => {
+    it('Must return a specific category', async () => {
+      const category = await service.findOne(
+        '1ca415c6-32be-488c-b7bf-12b8649c99bd',
+      );
+
+      expect(category).toEqual(categoryList[0]);
+      expect(repository.findOne).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should return NotFoundException error', async () => {
+      jest.spyOn(repository, 'findOne').mockResolvedValueOnce(undefined);
+
+      try {
+        await service.findOne('1');
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual(
+          `Não foi encontrado uma categoria com o ID : 1`,
+        );
+      }
+    });
+  });
+
+  describe('update', () => {
+    it('Must update a specific category', async () => {
+      jest.spyOn(repository, 'save').mockResolvedValueOnce(updatedCategory);
+
+      const medico = await service.update(
+        '1ca415c6-32be-488c-b7bf-12b8649c99bd',
+        { name: 'Sport' },
+      );
+
+      expect(medico).toEqual(updatedCategory);
+      expect(repository.preload).toHaveBeenCalledTimes(1);
+      expect(repository.save).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should return NotFoundException error', async () => {
+      jest.spyOn(repository, 'preload').mockResolvedValueOnce(undefined);
+
+      try {
+        await service.update('1', { name: 'Sport' });
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual(
+          `Não foi encontrado uma categoria com o ID : 1`,
+        );
+      }
+    });
+  });
+
+  describe('remove', () => {
+    it('Must delete a specific category', async () => {
+      const category = await service.remove(
+        '1ca415c6-32be-488c-b7bf-12b8649c99bd',
+      );
+
+      expect(category).toBe(categoryList[0]);
+      expect(repository.findOne).toHaveBeenCalledTimes(1);
+      expect(repository.remove).toHaveBeenCalledTimes(1);
+    });
   });
 });
