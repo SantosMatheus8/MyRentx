4f1445f936f8bbe2f2341ab459723c1facede280
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-11-10 09:35:52 -0300

    Criando os testes do controller de specification

--- src/specifications/specifications.controller.spec.ts
+++ src/specifications/specifications.controller.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateSpecificationDto } from './dto/create-specification.dto';
+import { UpdateSpecificationDto } from './dto/update-specification.dto';
 import { Specification } from './entities/specification.entity';
 import { SpecificationsController } from './specifications.controller';
 import { SpecificationsService } from './specifications.service';
@@ -46,5 +48,64 @@ describe('SpecificationsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('A specification must be created', async () => {
+      const body: CreateSpecificationDto = {
+        name: 'Specification1',
+        description: 'Specification Description1',
+      };
+
+      const res = await controller.create(body);
+
+      expect(res).toEqual(specificationList[0]);
+      expect(service.create).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findAll', () => {
+    it('Must list all categories', async () => {
+      const res = await controller.findAll();
+
+      expect(res).toEqual(specificationList);
+      expect(service.findAll).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findOne', () => {
+    it('Must return a specific specification', async () => {
+      const res = await controller.findOne(
+        '1ca415c6-32be-488c-b7bf-12b8649c99bd',
+      );
+
+      expect(res).toEqual(specificationList[0]);
+      expect(service.findOne).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('update', () => {
+    it('Must update a specific specification', async () => {
+      const body: UpdateSpecificationDto = { name: 'New name' };
+
+      const res = await controller.update(
+        '1ca415c6-32be-488c-b7bf-12b8649c99bd',
+        body,
+      );
+
+      expect(res).toEqual(updatedSpecification);
+      expect(service.update).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('remove', () => {
+    it('Must delete a specific specification', async () => {
+      const res = await controller.remove(
+        '1ca415c6-32be-488c-b7bf-12b8649c99bd',
+      );
+
+      expect(res).toEqual(specificationList[0]);
+      expect(service.remove).toHaveBeenCalledTimes(1);
+    });
   });
 });
